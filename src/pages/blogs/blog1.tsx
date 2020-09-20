f28eb00153dffc7d935a01dd9f344e02cbe47f77
@mas764 <mas764@cornell.edu>
2020-09-20 19:01:27 -0400

    start template for blog article

--- src/pages/blogs/blog1.tsx
+++ src/pages/blogs/blog1.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import styled from 'styled-components';
 import { Link } from 'gatsby';
+import { breakpoints } from '../../utils';
 
 import Layout from '../../components/layout';
 
-const IndexPage = () => <Layout>blog1</Layout>;
+const BlogWrapper = styled.div`
+  display: grid;
+  place-items: center;
+  margin: 5vh 15vh 0 15vh;
+  @media ${breakpoints.md} {
+    margin: 5vh 5vh 0 5vh;
+  }
+`;
+
+const IndexPage = () => (
+  <Layout>
+    <BlogWrapper>
+      <h2>Title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor dolorum
+        optio, dolores repellendus porro sapiente quod consequuntur dicta
+        tenetur, veritatis provident explicabo obcaecati ut! Exercitationem
+        illum illo vel aperiam repellendus.
+      </p>
+    </BlogWrapper>
+  </Layout>
+);
 
 export default IndexPage;
